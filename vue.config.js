f4f1c6770a0cbe8a4c2b2bdf7b14a87079f2e884
Desmin <chrisdalbano12@gmail.com>
2023-05-24 10:09:36 -0400

    modified vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,20 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   outputDir: 'dist',
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'My Awesome Website';
+      return args;
+    });
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico',
+    },
+  },
 });
